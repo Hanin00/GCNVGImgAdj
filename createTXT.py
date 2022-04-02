19362acc7838c57c0c024d0fd1fb85904573a818
DESKTOP-75V1S1U\Haeun <haeun5419@a.ut.ac.kr>
2022-04-03 01:05:22 +0900

    id x id Adj 를 사용하는 gcn의 전처리

--- createTXT.py
+++ createTXT.py
@@ -67,44 +67,41 @@ import json
 #         if i == j:
 #             adjM[i][j] += 1
 # np.save('idAdj.npy',adjM)
-#n = np.load('idAdj.npy')
+#idAdj = np.load('idAdj.npy')
 
-
-'''img 당 freObj 있/없 featuremap'''
-
-testFile = open('freObj.txt','r') # 'r' read의 약자, 'rb' read binary 약자 (그림같은 이미지 파일 읽을때)
-readFile = testFile.readline()
-list = (readFile[1:-1].replace("'",'')).replace(' ','').split(',')
-
-freObj = list[:100]
-
-featureMatrix = []
-with open('./data/scene_graphs.json') as file:  # open json file
-    data = json.load(file)
-    object = []
-    for i in range(1000):  # 이미지 1000개에 대한 각각의 objectNamesList 생성
-        objects = data[i]["objects"]
-        for j in range(len(objects)):  # 이미지의 object 개수만큼 반복
-            object.append(objects[j]['names'])  # 이미지 하나에 대한 objList
-
-       # object = sum(object, [])
-
-        # 이미지 하나의 obj랑 최빈 obj 랑 일치하는 게 있으면 1로 표시해서 특징 추출
-
-        row = [0 for i in range(len(freObj))]
-        l = 0
-        for k in range(len(freObj)) :
-            for j in range(len(objects)):
-                n = ''.join(object[j])
-                m = freObj[k]
-
-                if n in freObj :
-                    w = freObj.index(n)
-                    row[w] = 1
-
-        featureMatrix.append((row))
-featureMatrix =  np.array(featureMatrix)
-np.save('idFreFeature.npy',featureMatrix)
-d = np.load('idFreFeature.npy')
-
-print(d)
+#
+# '''img 당 freObj 있/없 featuremap'''
+# testFile = open('freObj.txt','r') # 'r' read의 약자, 'rb' read binary 약자 (그림같은 이미지 파일 읽을때)
+# readFile = testFile.readline()
+# list = (readFile[1:-1].replace("'",'')).replace(' ','').split(',')
+#
+# freObj = list[:100]
+#
+# featureMatrix = []
+# with open('./data/scene_graphs.json') as file:  # open json file
+#     data = json.load(file)
+#     object = []
+#     for i in range(1000):  # 이미지 1000개에 대한 각각의 objectNamesList 생성
+#         objects = data[i]["objects"]
+#         for j in range(len(objects)):  # 이미지의 object 개수만큼 반복
+#             object.append(objects[j]['names'])  # 이미지 하나에 대한 objList
+#
+#        # object = sum(object, [])
+#
+#         # 이미지 하나의 obj랑 최빈 obj 랑 일치하는 게 있으면 1로 표시해서 특징 추출
+#
+#         row = [0 for i in range(len(freObj))]
+#         l = 0
+#         for k in range(len(freObj)) :
+#             for j in range(len(objects)):
+#                 n = ''.join(object[j])
+#                 m = freObj[k]
+#
+#                 if n in freObj :
+#                     w = freObj.index(n)
+#                     row[w] = 1
+#
+#         featureMatrix.append((row))
+# featureMatrix =  np.array(featureMatrix)
+# np.save('idFreFeature.npy',featureMatrix)
+# featuremap = np.load('idFreFeature.npy')
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import util as ut
 import YEmbedding as yed
+import numpy as np
+
 
 '''
     adj = cluster 값이 동일한 img인 경우 인접
@@ -10,36 +12,51 @@ import YEmbedding as yed
 '''
 
 
-
-
-def print_hi(name):
-    return 0
-
-
+'''
+    featuremap = 1000x100(imgxfreObj)
+    idAdj(idxid)
+    label : img 1000개의 cluster 값(class,label로 사용)
+'''
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
-    startId = 1
-    endId = 1000
-    xlxspath = './data/image_regions.xlsx'
-    #Y - image, cluser 몇 번인지~
-    embedding_clustering = yed.YEmbedding(xlxspath)
-    idCluster = embedding_clustering[['image_id', 'cluster', 'distance_from_centroid']]
-    label = idCluster['cluster']  #<class 'pandas.core.series.Series'>
-
-    print(label)
-
-    ''' frequency obj - 1x100'''
-    #freObj = ut.prequency_feature(1, 1000)
+    featuremap = np.load('idFreFeature.npy')
+    idAdj = np.load('idAdj.npy')
     testFile = open('freObj.txt','r') # 'r' read의 약자, 'rb' read binary 약자 (그림같은 이미지 파일 읽을때)
     readFile = testFile.readline()
-    list = (readFile[1:-1].replace("'",'')).split(',')
+    label = (readFile[1:-1].replace("'",'')).split(',')
 
 
 
-    adjMatrix = ut.create_adjMatrix(clusterList=label[1])
-    # featuremap =ut.featuremap(startId,endId,freObj)
 
-print('freObj : ',freObj)
-print('adjMatrix : ', adjMatrix)
-# print('featuremap : ', featuremap)
+
+
+
+
+
+
+
+
+
+
+
+
+    # startId = 1
+    # endId = 1000
+    # xlxspath = './data/image_regions.xlsx'
+    # #Y - image, cluser 몇 번인지~
+    # embedding_clustering = yed.YEmbedding(xlxspath)
+    # idCluster = embedding_clustering[['image_id', 'cluster', 'distance_from_centroid']]
+    # label = idCluster['cluster']  #<class 'pandas.core.series.Series'>
+    #
+    # print(label)
+    #
+    # ''' frequency obj - 1x100'''
+    # #freObj = ut.prequency_feature(1, 1000)
+    # testFile = open('freObj.txt','r') # 'r' read의 약자, 'rb' read binary 약자 (그림같은 이미지 파일 읽을때)
+    # readFile = testFile.readline()
+    # list = (readFile[1:-1].replace("'",'')).split(',')
+    #
+    #
+    #
+    # adjMatrix = ut.create_adjMatrix(clusterList=label[1])
+    # # featuremap =ut.featuremap(startId,endId,freObj)
