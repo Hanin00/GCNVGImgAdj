3f98c0d425277d646fc5578c1ae046175d7a0ead
DESKTOP-75V1S1U\Haeun <haeun5419@a.ut.ac.kr>
2022-04-04 21:45:10 +0900

    GNN NodeClassification id x id, id x freObj

--- d.py
+++ d.py
@@ -1,11 +1,15 @@
 import sys
+import numpy as np
 
 
-testFile = open('./data/cluster.txt', 'r')  # 'r' read의 약자, 'rb' read binary 약자 (그림같은 이미지 파일 읽을때)
-readFile = testFile.readline()
-label = (readFile[1:-1].replace("'", '').replace(' ', '')).split(',')
-label = label[:1000]  # 빈출 100 단어 만 사용
+j = np.load('./data/idFreFeature.npy')
+print(j)
 
-print(len(label))
-print(label[:-20])
-print(label[:20])
+# testFile = open('./data/cluster.txt', 'r')  # 'r' read의 약자, 'rb' read binary 약자 (그림같은 이미지 파일 읽을때)
+# readFile = testFile.readline()
+# label = (readFile[1:-1].replace("'", '').replace(' ', '')).split(',')
+# label = label[:1000]  # 빈출 100 단어 만 사용
+#
+# print(len(label))
+# print(label[:-20])
+# print(label[:20])
