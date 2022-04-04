26924a22081b46febf2edd7f21951220202b39cb
DESKTOP-75V1S1U\Haeun <haeun5419@a.ut.ac.kr>
2022-04-05 00:09:11 +0900

    Node Classification 결과로 다른 범위의 이미지 유사 여부 판별... 유사 여부 판별은 단순 비교이므로 해당 결과가 모델에 반영되도록 변경 필요 및 해당 모델은 relationship이 반여오디지 않았으므로 해당 부분 추가적인 보충 필요

--- GNNNodeClassification.py
+++ GNNNodeClassification.py
@@ -145,8 +145,8 @@ class GCN(nn.Module):
         x = F.relu(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
-        return F.log_softmax(x, dim=1)
 
+        return F.log_softmax(x, dim=1)
 
 n_labels = labels.max().item() + 1  # 15
 n_features = features.shape[1]      # 100
@@ -212,221 +212,76 @@ for i in tnrange(epochs):
 
 output = model(features, adj)
 
-
+#test
 samples = 10
 #torch.randperm : 데이터 셔플
+#idx_sample = idx_test[torch.randperm(len(idx_test))[:samples]]
 idx_sample = idx_test[torch.randperm(len(idx_test))[:samples]]
+print(idx_sample)
 
-idx2lbl = ['1번 그림', '2번 그림', '3번 그림', '4번 그림', '5번 그림', '6번 그림', '7번 그림'
-, '8번 그림', '9번 그림', '10번 그림', '11번 그림', '12번 그림', '13번 그림', '14번 그림','15번 그림']
+idx2lbl = ['0번 그림','1번 그림', '2번 그림', '3번 그림', '4번 그림', '5번 그림', '6번 그림', '7번 그림'
+, '8번 그림', '9번 그림', '10번 그림', '11번 그림', '12번 그림', '13번 그림', '14번 그림']
 #실제 라벨 10개, predicate 결과 10개
+
+#numpy.argmax() : 주어진 배열에서 가장 높은 값을 가진 인덱스 반환
+#pd.tolist() : 동일 위치(레벨)에 있는 데이터끼리 묶어줌
 df = pd.DataFrame({'Real': [idx2lbl[e] for e in labels[idx_sample].tolist()],
                    'Pred': [idx2lbl[e] for e in output[idx_sample].argmax(1).tolist()]})
+# [idx2lbl[e] for e in output[idx_sample].argmax(1).tolist()] : 예측한 값이 label의 어디에 속하는지
+# Pred
+# idx_sample 개수만큼의 output을 가져오는데, argmax로 output 값 15개(각 cluster에 대한 값) 중 가장 큰 값을 뽑고, 그 위치를 받음
+# tolist()로 각 feature 별 model의 output 값을 받음
+# for 문을 통해 idx2Lble에서 해당하는 label을 반환받음.
 print(df)
 
+#
+# print(idx2lbl[e] for e in output[10].argmax(1).tolist())
 
 
 
 
+'''
+    각 범위 별 이미지 값으로 동일 여부 확인
+    범위 추가하는 변수2개 만들면 될 듯
+    -> 이 T/F 값을 model에 반영 시키는 방법?
+'''
+
+idx_sample1 = idx_test[torch.randperm(len(idx_test))[1:10]]
+realList1 = [idx2lbl[e] for e in labels[idx_sample1].tolist()]
+predList1 = [idx2lbl[e] for e in output[idx_sample1].argmax(1).tolist()]
+
+idx_sample2 = idx_test[torch.randperm(len(idx_test))[22:31]]
+realList2 = [idx2lbl[e] for e in labels[idx_sample2].tolist()]
+predList2 = [idx2lbl[e] for e in output[idx_sample2].argmax(1).tolist()]
+
+resPred = []
+for i in range(len(predList2)) :
+    if(predList1[i] == predList2[i]) :
+        resPred.append('T')
+    else :
+        resPred.append('F')
+
+resReal = []
+for i in range(len(realList1)) :
+    if(realList1[i] == realList2[i]) :
+        resReal.append('T')
+    else :
+        resReal.append('F')
+
+#img_id 값으로 그림 확인하고 싶어서 id 찍어봄
+print("idx_sample1_Imgid : ",idx_sample1)
+print("idx_sample2_Imgid : ", idx_sample2)
+idDF1 = pd.DataFrame({'idx_sample1_Imgid': idx_sample1,
+                   'idx_sample2_Imgid': idx_sample2})
+print(idDF1)
 
 
-# device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-# path = Path('./data/cora')
-#
-# paper_features_label = np.genfromtxt(path/'cora.content', dtype=np.str)
-#
-# features = csr_matrix(paper_features_label[:, 1:-1], dtype=np.float32)
-# labels = paper_features_label[:, -1]
-# lbl2idx = {k:v for v,k in enumerate(sorted(np.unique(labels)))}
-# labels = [lbl2idx[e] for e in labels]
-#
-# papers = paper_features_label[:,0].astype(np.int32)
-#
-# paper2idx = {k:v for v,k in enumerate(papers)}
-# edges = np.genfromtxt(path/'cora.cites', dtype=np.int32)
-# edges = np.asarray([paper2idx[e] for e in edges.flatten()], np.int32).reshape(edges.shape)
-#
-# adj = coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
-#                  shape=(len(labels), len(labels)), dtype=np.float32)
-#
-# adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
-#
-# def normalize(mx):
-#     rowsum = np.array(mx.sum(1))
-#     r_inv = (rowsum ** -1).flatten()
-#     r_inv[np.isinf(r_inv)] = 0.
-#     r_mat_inv = diags(r_inv)
-#     mx = r_mat_inv.dot(mx)
-#     return mx
-#
-# features = normalize(features)
-#
-# #eye : 단위행렬 만드는 함수
-# adj = normalize(adj + eye(adj.shape[0])) #Normalizing for removing gradient vanishing and exploding problem
-# #normarlize(원래 adj + 단위행렬(자기 자신 가리킴))
-#
-# #todensd() : Scipy 압축 희소행(CSR) 행렬을 Numpy() 행렬로 변환
-# adj = torch.FloatTensor(adj.todense())
-#
-# features = torch.FloatTensor(features.todense()) #scipy.sparse -> numpy.matrix->torch.tensor
-# labels = torch.LongTensor(labels)
-#
-#
-#
-# # dataset train/test/val로 나눔
-# np.random.seed(34)
-# n_train = 200
-# n_val = 300
-# n_test = len(features) - n_train - n_val
-# idxs = np.random.permutation(len(features))
-# idx_train = torch.LongTensor(idxs[:n_train])
-# idx_val   = torch.LongTensor(idxs[n_train:n_train+n_val])
-# idx_test  = torch.LongTensor(idxs[n_train+n_val:])
-#
-# #cuda.. gpu로 보냄
-# adj = adj.to(device)
-# features = features.to(device)
-# labels = labels.to(device)
-# idx_train = idx_train.to(device)
-# idx_val = idx_val.to(device)
-# idx_test = idx_test.to(device)
-#
-# # Model
-# class GraphConvolution(Module):
-#     def __init__(self, in_features, out_features, bias=True):
-#         super(GraphConvolution, self).__init__()
-#         self.in_features = in_features
-#         self.out_features = out_features
-#
-#         # weight를 reset해 줌. weight 변화에 따른 정확도 측정을 위해서? 왜하지? 이유 찾아보기
-#         self.weight = Parameter(torch.FloatTensor(in_features, out_features))
-#         if bias:
-#             self.bias = Parameter(torch.FloatTensor(out_features))
-#         else:
-#             self.register_parameter('bias', None)
-#         self.reset_parameters()
-#
-#     def reset_parameters(self):
-#         stdv = 1. / math.sqrt(self.weight.size(1))
-#         self.weight.data.uniform_(-stdv, stdv)
-#         if self.bias is not None:
-#             self.bias.data.uniform_(-stdv, stdv)
-#         #
-#
-#     def forward(self, input, adj):
-#         support = torch.mm(input, self.weight)
-#         output = torch.spmm(adj, support)
-#         if self.bias is not None:
-#             return output + self.bias
-#         else:
-#             return output
-#
-#     def __repr__(self):
-#         return self.__class__.__name__ + ' (' \
-#                + str(self.in_features) + ' -> ' \
-#                + str(self.out_features) + ') '
-#
-#
-# class GCN(nn.Module):
-#     def __init__(self, nfeat, nhid, nclass, dropout):
-#         super(GCN, self).__init__()
-#
-#         self.gc1 = GraphConvolution(nfeat, nhid)
-#         self.gc2 = GraphConvolution(nhid, nclass)
-#         self.dropout = dropout
-#
-#     # X : 초기 랜덤값 -> 학습 하면서 변경
-#     def forward(self, x, adj):
-#         x = F.relu(self.gc1(x, adj))
-#         x = F.dropout(x, self.dropout, training=self.training)
-#         x = self.gc2(x, adj)
-#         return F.log_softmax(x, dim=1)
-#
-#
-# n_labels = labels.max().item() + 1  # 7
-# n_features = features.shape[1]      # 1433
-#
-# #seed 고정
-# torch.manual_seed(34)
-#
-# # model
-# model = GCN(nfeat=n_features,
-#             nhid=20,  # hidden = 16
-#             nclass=n_labels,
-#             dropout=0.5)  # dropout = 0.5
-# optimizer = optim.Adam(model.parameters(),
-#                        lr=0.001, weight_decay=5e-4)
-#
-# def step():
-#     t = time.time()
-#     model.train()  #model 학습모드로
-#     optimizer.zero_grad()
-#     output = model(features, adj)  #model에 값 넣음
-#     loss = F.nll_loss(output[idx_train], labels[idx_train]) #loss 함수
-#     acc = accuracy(output[idx_train], labels[idx_train]) #accuracy 파악
-#     loss.backward()
-#     optimizer.step()
-#
-#     return loss.item(), acc
-#
-# #평가
-# def evaluate(idx):
-#     model.eval()
-#     output = model(features, adj) #모델 돌림
-#     loss = F.nll_loss(output[idx], labels[idx]) #모델이 분류한 값과 label 비교해서 loss 파악
-#     acc = accuracy(output[idx], labels[idx])
-#
-#     return loss.item(), acc
-#
-# #정확도
-# def accuracy(output, labels):
-#     preds = output.max(1)[1].type_as(labels) # 비슷하다고 뽑은 것들중에 제일 비슷한 거
-#     correct = preds.eq(labels).double()
-#     correct = correct.sum()
-#     return correct / len(labels)
-#
-#
-# epochs = 1000
-# print_steps = 100
-# train_loss, train_acc = [], []
-# val_loss, val_acc = [], []
-#
-# for i in tnrange(epochs):
-#     tl, ta = step()
-#     train_loss += [tl]
-#     train_acc += [ta]
-#
-#     if ((i + 1) % print_steps) == 0 or i == 0:
-#         tl, ta = evaluate(idx_train)
-#         vl, va = evaluate(idx_val)
-#         val_loss += [vl]
-#         val_acc += [va]
-#
-#         print('Epochs: {}, Train Loss: {:.3f}, Train Acc: {:.3f}, Validation Loss: {:.3f}, Validation Acc: {:.3f}'.format(i, tl, ta, vl, va))
-#
-# '''
-# fig, axes = plt.subplots(1,2, figsize=(15,5))
-#
-# ax = axes[0]
-# axes[0].plot(train_loss[::print_steps] + [train_loss[-1]], label='Train')
-# axes[0].plot(val_loss, label='Validation')
-# axes[1].plot(train_acc[::print_steps] + [train_acc[-1]], label='Train')
-# axes[1].plot(val_acc, label='Validation')
-# axes[0].grid()
-# axes[1].grid()
-#
-# for ax, t in zip(axes, ['Loss','Accuracy']):
-#     ax.legend(), ax.set_title(t, size=15)'''
-#
-# output = model(features, adj)
-# print("output : ",output)
-#
-# samples = 10
-# #torch.randperm : 데이터 셔플
-# idx_sample = idx_test[torch.randperm(len(idx_test))[:samples]]
-#
-# idx2lbl = {v:k for k,v in lbl2idx.items()}
-# #실제 라벨 10개, predicate 결과 10개
-# df = pd.DataFrame({'Real': [idx2lbl[e] for e in labels[idx_sample].tolist()],
-#                    'Pred': [idx2lbl[e] for e in output[idx_sample].argmax(1).tolist()]})
-# print(df)
+df1 = pd.DataFrame({'Pre1': [idx2lbl[e] for e in output[idx_sample1].argmax(1).tolist()],
+                   'Pred2': [idx2lbl[e] for e in output[idx_sample2].argmax(1).tolist()],
+                   'Res(T/F)':resPred})
+df2 = pd.DataFrame({'Real1':  [idx2lbl[e] for e in labels[idx_sample1].tolist()],
+                   'Real2':  [idx2lbl[e] for e in labels[idx_sample2].tolist()],
+                   'Res(T/F)':resReal})
+
+print(df1)
+print(df2)
